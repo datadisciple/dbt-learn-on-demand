60619632cb750352c510a958930fc71cf82da0cf
Sean Meehan <sean.meehan22@gmail.com>
2021-07-19 05:34:52 +0000

    cleans staging table sql to remove unnecessary WITH statement

--- models/staging/jaffle_shop/stg_customers.sql
+++ models/staging/jaffle_shop/stg_customers.sql
@@ -1,12 +1,7 @@
-with customers as (
+select
+    id as customer_id,
+    first_name,
+    last_name
 
-    select
-        id as customer_id,
-        first_name,
-        last_name
+from `dbt-tutorial`.jaffle_shop.customers
 
-    from `dbt-tutorial`.jaffle_shop.customers
-
-)
-
-select * from customers
