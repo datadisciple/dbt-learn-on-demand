9fa6de6adc560fa263e950163747d8d2be640889
Sean Meehan <sean.meehan22@gmail.com>
2021-07-19 04:22:06 +0000

    introduces modularity & shows how to use ref() to build dependencies

--- models/dim_customers.sql
+++ models/dim_customers.sql
@@ -1,26 +1,11 @@
 {{ config (materialized = "table")}}    -- tells dbt to build as a table (instead of a view)
 
 with customers as (
-
-    select
-        id as customer_id,
-        first_name,
-        last_name
-
-    from `dbt-tutorial`.jaffle_shop.customers
-
+    select * from {{ ref('stg_customers') }}    -- ref() allows you to build dependencies b/w models
 ),
 
 orders as (
-
-    select
-        id as order_id,
-        user_id as customer_id,
-        order_date,
-        status
-
-    from `dbt-tutorial`.jaffle_shop.orders
-
+    select * from {{ ref('stg_orders') }}
 ),
 
 customer_orders as (
